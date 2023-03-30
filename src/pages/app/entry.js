d99ba66fb686adb8a8d98c004e64aec12e28f7a4
Dilip Bharvadiya <ahirdilp@gmail.com>
2023-03-30 14:40:30 +0530

    first

---
+++ src/pages/app/entry.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '@/styles/Entry.module.css'
+
+const Entry = () => {
+  return (
+    <div className={styles.cont}>Entry</div>
+  )
+}
+
+export default Entry

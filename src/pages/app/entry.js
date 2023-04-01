076b475b113f17a391ef37a4737c76eff8ce76d6
dlipahir <ahirdilp@gmail.com>
2023-04-01 09:14:22 +0530

    added entry

--- src/pages/app/entry.js
+++ src/pages/app/entry.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/styles/Entry.module.css'
-import { getTodayEntries } from '../../utils/firebaseConfig'
+import { getTodayEntry } from '../../utils/firebaseConfig'
+import Image from 'next/image'
 
 const Entry = () => {
 
+  const [data, setData] = useState([]);
+  console.log("out", data);
+
+  const getData = async () => {
+    const ent = await getTodayEntry();
+    setData(ent);
+  }
+
+
+  useEffect(() => {
+    console.log("in");
+    getData();
+  }, [])
+
   return (
     <div className={styles.cont}>
-     <div className={styles.row}>
-      <div className={styles.col}>Name</div>
-      <div className={styles.col}>Room No.</div>
-      <div className={styles.col}>Room No.</div>
-     </div>
+      <div className={styles.row}>
+        <div className={styles.col}>Enrollment No.</div>
+        <div className={styles.col}>Last Entry</div>
+        <div className={styles.col}>current status</div>
+        <div className={styles.col}>Info</div>
+       
+      </div>
+
+      {
+        data.map((item, i) => {
+          return (
+
+            <div className={styles.row}>
+              <div className={styles.col}>{item.id}</div>
+              <div className={styles.col}>{
+                new Date(item.value.time.seconds * 1000).toString().split(" ")[4]
+              }</div>
+              <div className={styles.col}>
+                <div className={styles.dot} style={{background : item.value.checkIn ? "green" : "red"}}></div>
+
+              </div>
+              <div className={styles.col}>
+                
+                <Image src={require('../../assets/img/info.png')} alt="" srcset="" height={20}/>
+              </div>
+
+
+            </div>
+          )
+        })}
+
     </div>
   )
 }
--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -21,216 +21,314 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-const getTodayEntries = async()=>{
-    const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
-    const querySnapshot = await getDocs(query(collection(db, "entry","20230330","20230330"), limit(2)));
-    //query(citiesRef, orderBy("name"), limit(3))
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
+export const getTodayEntry = async()=>{
+  const data = [];
+  var d = new Date();
+  const today =
+      d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2);
+  console.log(today);
+  const querySnapshot = await getDocs(query(collection(db, "entry", today,"student"), limit(10)));
 
-
-
-
-  //   console.log(today);
-  //    const docRef = doc(db, "entry", "20230330");
-  //   const data  = await getDoc(docRef);
-
-  //   if (data.exists()) {
-  //       console.log("Document data:", data.data());
-  //     } else {
-  //       // doc.data() will be undefined in this case
-  //       console.log("No such document!");
-  //     }
-   }
-const getTodayEntries2 = async()=>{
-    const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
-    const querySnapshot = await getDocs(query(collection(db, "entry","20230328","200170107125"), limit(2)));
-    //query(citiesRef, orderBy("name"), limit(3))
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
-
-
-
-
-  //   console.log(today);
-  //    const docRef = doc(db, "entry","20230328","200170107124");
-  //   const data  = await getDoc(docRef);
-
-  //   if (data.exists()) {
-  //       console.log("Document data:", data.data());
-  //     } else {
-  //       doc.data() will be undefined in this case
-  //       console.log("No such document!");
-  //     }
-   }
-const getTodayEntries3 = async()=>{
-    // const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
-    // const querySnapshot = await getDocs(query(collection(db, "entry","20230328"), limit(2)));
-    // //query(citiesRef, orderBy("name"), limit(3))
-    // querySnapshot.forEach((doc) => {
-    //   // doc.data() is never undefined for query doc snapshots
-    //   console.log(doc.id, " => ", doc.data());
-    // });
-
-
-
-
-    //console.log(today);
-     const docRef = doc(db, "entry","20230331");
-    const data  = await getDoc(docRef);
-
-    if (data.exists()) {
-        console.log("Document data:", data.data());
-      } else {
-        doc.data();
-        // will be undefined in this case
-        console.log("No such document!");
-      }
-   }
-const getTodayEntries4 = async()=>{
-    // const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
-    const querySnapshot = await getDocs(query(collection(db, "student"), where("checkIn", "==", true)));
-    //query(citiesRef, orderBy("name"), limit(3))
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
-
-
-
-   }
-
-
-   getTodayEntries4();
-
-
-
- const checkStudentActivity = async (enroll) => {
-  console.log("yae")
-  const checkRef = doc(db, "student", `${enroll}`);
-  const docSnap = await getDoc(checkRef);
   
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+    data.push({id:doc.id,value:doc.data()});
+  });
+  console.log("dat",data.length);
+  return data;
   
-  if (docSnap.exists()) {
-    //  console.log("Document data:", docSnap.data());
-    return docSnap.data();
-  } else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-  }
-  return docSnap.data();
 
-  // await setDoc(doc(db, "cities", "LA"), {
-  //   name: "Los Angeles",
-  //   state: "CA",
-  //   country: "USA"
-  // });
-};
+}
+//getTodayEntry();
 
- const setNewStudent = async (enroll,time,checkIn=true) => {
-  const docSnap = await setDoc(doc(db, "student", enroll), {
-    checkIn,time
-  });
- // console.log(docSnap);
-  addActivity(enroll,time,checkIn);
+const getStudentEntryBydate = async(enroll)=>{
+  var d = new Date();
+  const today =
+      d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2) + ("0" + d.getDate()).slice(-2);
+      console.log(today);
+      const querySnapshot = await getDocs(query(collection(db, "entry", today,enroll), limit(10)));
+
+
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
 }
 
-const addActivity  = async (enroll,time,checkIn=true)=>{
-  const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
-  const docRef = await addDoc(collection(db, "entry",today,today), {
-    checkIn,
-    time: Timestamp.now(),
-    enroll
-  });
-  console.log("Document written with ID: ", docRef.id);
-}
+//getStudentEntryBydate("200170107124");
 
-const handleMain = async (enroll) => {
-  const data = await checkStudentActivity(enroll);
-  const time = Timestamp.now();
-  console.log(data);
-  if (data == undefined) {
-    console.log("add new data");
-    setNewStudent(enroll,time);
-  } else {
-    setNewStudent(enroll,time,!data.checkIn);
-  }
-};
 
-module.exports={
-  getTodayEntries,
-}
 
-//handleMain("200170107125");
 
-// async function  writeUserData(enroll, name)  {
-//     const db = getDatabase();
-//   await  set(ref(db, 'users/' + enroll), {
-//       name: name,
-//       entry:true
+
+
+
+
+
+
+
+
+
+// const mainput = (enroll)=>{
+//   const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+  
+
+
+
+// }
+
+
+// const addActivity  = async (enroll,time,checkIn=true,today)=>{
+//   const docRef = await addDoc(collection(db, "entry",today,enroll), {
+//     checkIn,
+//     time: Timestamp.now(),
+//     enroll
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// }
+// const setActivity  = async (enroll,time,checkIn=true,today)=>{
+
+//   console.log("Document written with ID: ", docRef.id);
+//   const docSnap = await setDoc(doc(db, "student", enroll), {
+//     checkIn,time
+//   });
+//   const docSnap1 = await setDoc(doc(db, "entry", today,"student",enroll), {
+//     checkIn,time
+//   });
+// }
+
+
+
+
+
+
+
+// const getTodayEntries = async()=>{
+//     const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+//     const querySnapshot = await getDocs(query(collection(db, "entry","20230330","20230330"), limit(2)));
+//     //query(citiesRef, orderBy("name"), limit(3))
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
 //     });
-//     console.log("hello")
+
+
+
+
+//   //   console.log(today);
+//   //    const docRef = doc(db, "entry", "20230330");
+//   //   const data  = await getDoc(docRef);
+
+//   //   if (data.exists()) {
+//   //       console.log("Document data:", data.data());
+//   //     } else {
+//   //       // doc.data() will be undefined in this case
+//   //       console.log("No such document!");
+//   //     }
+//    }
+// const getTodayEntries2 = async()=>{
+//     const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+//     const querySnapshot = await getDocs(query(collection(db, "entry","20230328","200170107125"), limit(2)));
+//     //query(citiesRef, orderBy("name"), limit(3))
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
+//     });
+
+
+
+
+//   //   console.log(today);
+//   //    const docRef = doc(db, "entry","20230328","200170107124");
+//   //   const data  = await getDoc(docRef);
+
+//   //   if (data.exists()) {
+//   //       console.log("Document data:", data.data());
+//   //     } else {
+//   //       doc.data() will be undefined in this case
+//   //       console.log("No such document!");
+//   //     }
+//    }
+// const getTodayEntries3 = async()=>{
+//     // const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+//     // const querySnapshot = await getDocs(query(collection(db, "entry","20230328"), limit(2)));
+//     // //query(citiesRef, orderBy("name"), limit(3))
+//     // querySnapshot.forEach((doc) => {
+//     //   // doc.data() is never undefined for query doc snapshots
+//     //   console.log(doc.id, " => ", doc.data());
+//     // });
+
+
+
+
+//     //console.log(today);
+//      const docRef = doc(db, "entry","20230331");
+//     const data  = await getDoc(docRef);
+
+//     if (data.exists()) {
+//         console.log("Document data:", data.data());
+//       } else {
+//         doc.data();
+//         // will be undefined in this case
+//         console.log("No such document!");
+//       }
+//    }
+// const getTodayEntries4 = async()=>{
+//     // const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+//     const querySnapshot = await getDocs(query(collection(db, "student"), where("checkIn", "==", true)));
+//     //query(citiesRef, orderBy("name"), limit(3))
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+//       console.log(doc.id, " => ", doc.data());
+//     });
+
+
+
+//    }
+
+
+//    getTodayEntries4();
+
+
+
+//  const checkStudentActivity = async (enroll) => {
+//   console.log("yae")
+//   const checkRef = doc(db, "student", `${enroll}`);
+//   const docSnap = await getDoc(checkRef);
+  
+  
+//   if (docSnap.exists()) {
+//     //  console.log("Document data:", docSnap.data());
+//     return docSnap.data();
+//   } else {
+//     // doc.data() will be undefined in this case
+//     console.log("No such document!");
 //   }
+//   return docSnap.data();
 
-//  async function getValue(enroll){
-//     const db = getDatabase();
+//   // await setDoc(doc(db, "cities", "LA"), {
+//   //   name: "Los Angeles",
+//   //   state: "CA",
+//   //   country: "USA"
+//   // });
+// };
 
-//     const dbRef = ref(db);
-//  const itm =  await  get(child(dbRef,`users/${enroll}`));
-//  console.log("data1",itm.toJSON())
-//  return itm.toJSON();
+//  const setNewStudent = async (enroll,time,checkIn=true) => {
+//   const docSnap = await setDoc(doc(db, "student", enroll), {
+//     checkIn,time
+//   });
+//  // console.log(docSnap);
+//   addActivity(enroll,time,checkIn);
+// }
 
-// //  .then((snapshot) => {
-// //       if (snapshot.exists()) {
-// //         console.log(snapshot.val());
-// //         const data= snapshot.val();
-// //         console.log(data);
-// //         return data;
-// //       } else {
-// //         console.log("No data available");
-// //         return false;
-// //       }
-// //     }).catch((error) => {
-// //       console.error(error);
-// //       return false;
+// const addActivity  = async (enroll,time,checkIn=true)=>{
+//   const today = new Date().toISOString().slice(0, 10).replaceAll('-','');
+//   const docRef = await addDoc(collection(db, "entry",today,enroll), {
+//     checkIn,
+//     time: Timestamp.now(),
+//     enroll
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// }
+// const setActivity  = async (enroll,time,checkIn=true,today)=>{
+
+//   console.log("Document written with ID: ", docRef.id);
+//   const docSnap = await setDoc(doc(db, "student", enroll), {
+//     checkIn,time
+//   });
+//   const docSnap1 = await setDoc(doc(db, "entry", today,"student",enroll), {
+//     checkIn,time
+//   });
+// }
+
+
+
+
+
+
+// const handleMain = async (enroll) => {
+//   const data = await checkStudentActivity(enroll);
+//   const time = Timestamp.now();
+//   console.log(data);
+//   if (data == undefined) {
+//     console.log("add new data");
+//     setNewStudent(enroll,time);
+//   } else {
+//     setNewStudent(enroll,time,!data.checkIn);
+//   }
+// };
+
+// module.exports={
+//   getTodayEntries,
+// }
+
+// //handleMain("200170107125");
+
+// // async function  writeUserData(enroll, name)  {
+// //     const db = getDatabase();
+// //   await  set(ref(db, 'users/' + enroll), {
+// //       name: name,
+// //       entry:true
 // //     });
+// //     console.log("hello")
+// //   }
 
-//  }
+// //  async function getValue(enroll){
+// //     const db = getDatabase();
 
-//  const updateEntry = (enroll,data)=>{
-//     const db = getDatabase();
+// //     const dbRef = ref(db);
+// //  const itm =  await  get(child(dbRef,`users/${enroll}`));
+// //  console.log("data1",itm.toJSON())
+// //  return itm.toJSON();
 
-//     const dbRef = ref(db);
-//     const updates = {};
-//     const newentry = !data.entry;
-//     console.log("ent" , data.entry,newentry)
-//   updates[`users/${enroll}/entry`] = !data.entry;
-//  update(dbRef, updates);
-//   console.log("entry updated");
+// // //  .then((snapshot) => {
+// // //       if (snapshot.exists()) {
+// // //         console.log(snapshot.val());
+// // //         const data= snapshot.val();
+// // //         console.log(data);
+// // //         return data;
+// // //       } else {
+// // //         console.log("No data available");
+// // //         return false;
+// // //       }
+// // //     }).catch((error) => {
+// // //       console.error(error);
+// // //       return false;
+// // //     });
 
-//  }
+// //  }
 
-//   //writeUserData("200170107124","Dilip").then((data)=>{console.log})
+// //  const updateEntry = (enroll,data)=>{
+// //     const db = getDatabase();
 
-//  //getValue();
-//  //updateEntry("200170107124");
+// //     const dbRef = ref(db);
+// //     const updates = {};
+// //     const newentry = !data.entry;
+// //     console.log("ent" , data.entry,newentry)
+// //   updates[`users/${enroll}/entry`] = !data.entry;
+// //  update(dbRef, updates);
+// //   console.log("entry updated");
 
-//  const mainHandle =async (enroll)=>{
-//     const data = await getValue(enroll);
-//     console.log("data",data);
+// //  }
 
-//     if(data==null){
-//         writeUserData(enroll,"qwerty");
-//         console.log("you are checked in");
-//         return;
-//     }
-//     else{
-//         updateEntry(enroll,data);
-//     }
-//  }
+// //   //writeUserData("200170107124","Dilip").then((data)=>{console.log})
 
-//  mainHandle("200170107136");
+// //  //getValue();
+// //  //updateEntry("200170107124");
+
+// //  const mainHandle =async (enroll)=>{
+// //     const data = await getValue(enroll);
+// //     console.log("data",data);
+
+// //     if(data==null){
+// //         writeUserData(enroll,"qwerty");
+// //         console.log("you are checked in");
+// //         return;
+// //     }
+// //     else{
+// //         updateEntry(enroll,data);
+// //     }
+// //  }
+
+// //  mainHandle("200170107136");
